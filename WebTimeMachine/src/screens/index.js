0621d6cb8937758637e7af92e667ce2b83d93de3
benedicteb2054 <60343180+benedicteb2054@users.noreply.github.com>
2022-07-30 23:11:48 +0200

    Delete WebTimeMachine directory

--- WebTimeMachine/src/screens/index.js
+++
@@ -1,4 +0,0 @@
-export { default as LoginScreen } from './LoginScreen';
-export { default as ChooseWebSite } from './ChooseWebSite';
-export { default as InfoScreen } from './InfoScreen';
-export { default as AuthLoadingScreen } from './AuthLoadingScreen';
